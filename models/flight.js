97c67d36e9e6c2854a631850e0bf2d0617578e6d
Michael Grebowicz <maciejgrebowicz@gmail.com>
2020-11-22 19:55:28 -0800

    create flightSchema in models/flight.js

---
+++ models/flight.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX','SAN'],
+        default: 'DEN'
+    },
+
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+
+    departs: {
+        type: Date,
+        default: function() {
+            let nextYear = new Date();
+            return nextYear.setFullYear(nextYear.getFullYear() + 1);
+        }
+    }
+
+}, {
+    timestamps: true
+});
