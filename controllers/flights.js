ffb9ee1afb06e7fd0e5bf3fab28abb26951a8da0
Michael Grebowicz <maciejgrebowicz@gmail.com>
2020-11-25 22:28:32 -0800

    enable new and create functionality, add temporary styling

--- controllers/flights.js
+++ controllers/flights.js
@@ -9,17 +9,24 @@ module.exports = {
 
 
 function create(req, res) {
-
+    const flight = new Flight(req.body);
+    flight.save(function(err) {
+        if (err) return res.render('flights/new');
+        console.log(flight);
+        res.redirect('flights/new');
+    });
 };
 
 function show(req, res) {
 
 };
 
-function newFlight(req, res) {
+function newFlight(req, res) {    
     res.render('flights/new');
 };
 
 function index(req, res) {
-    res.render('flights/index');
+    Flight.find({}, function (err, flights) {
+        res.render('flights/index', { title: 'All Flights', flights});
+    });
 };
